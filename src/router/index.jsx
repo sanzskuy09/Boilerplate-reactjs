b33c20189ae4a55a10013021f90dfad68de120a4
sanzskuy09 <mn.ihsan09@gmail.com>
2024-01-10 19:40:55 +0700

    sorting folder

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,52 +1,10 @@
 import { useState } from "react";
-import { Navigate, useNavigate, useRoutes } from "react-router-dom";
+import { Navigate, useRoutes } from "react-router-dom";
 
 import Homepage from "@/pages/Homepage";
 import About from "@/pages/About";
+import Login from "@/pages/Login";
 import Page404 from "@/pages/404";
-
-function Login({ onLogin }) {
-  const [creds, setCreds] = useState({});
-  const navigate = useNavigate();
-
-  function handleLogin() {
-    if (creds.username === "admin" && creds.password === "123") {
-      onLogin && onLogin({ username: creds.username });
-      navigate("/stats");
-    }
-  }
-
-  function logOut() {
-    onLogin(null);
-    navigate("/");
-  }
-
-  return (
-    <div style={{ padding: 10 }}>
-      <br />
-      <span>Username:</span>
-      <br />
-      <input
-        type="text"
-        onChange={(e) => setCreds({ ...creds, username: e.target.value })}
-      />
-      <br />
-      <span>Password:</span>
-      <br />
-      <input
-        type="password"
-        onChange={(e) => setCreds({ ...creds, password: e.target.value })}
-      />
-      <br />
-      <br />
-      <button onClick={handleLogin}>Login</button>
-
-      <span onClick={logOut} style={{ padding: 5, cursor: "pointer" }}>
-        Logout
-      </span>
-    </div>
-  );
-}
 
 export default function Routes() {
   const [user, setUser] = useState();
