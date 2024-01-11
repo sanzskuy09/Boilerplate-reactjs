e2f6a2f7c5cb1aa821297be66c4b9a30c11c4cdf
sanzskuy09 <mn.ihsan09@gmail.com>
2024-01-11 07:06:44 +0700

    add framer motion

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Homepage = () => {
   return (
-    <div style={{ padding: 20 }}>
+    <div style={{ padding: 20 }} className="bg-red-500">
       <h2>Home View</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+
+      <motion.div
+        className="w-14 h-14 bg-white rounded-xl"
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.8 }}
+      />
     </div>
   );
 };
